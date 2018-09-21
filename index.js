8bcc7c25b9a2988f4698a47f3933a05243f8c36b
Ole Martin Ruud <barskern@outlook.com>
2018-09-21 19:45:37 +0200

    Setup simple preact project

---
+++ index.js
@@ -0,0 +1,15 @@
+import { Component } from 'preact'
+import { Button } from 'preact-fluid'
+
+import './style'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+                <Button>Press me</Button>
+            </div>
+        );
+    }
+}

56e707536eeaa4aec0321f3c781fbab91f91775c
Ole Martin Ruud <barskern@outlook.com>
2018-09-22 05:01:08 +0200

    Setup redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 
 import Router from 'preact-router'
+import { Provider } from 'preact-redux'
+import { createStore } from 'redux'
+import reducers from './reducers/'
 
 import './style'
 
@@ -7,11 +10,15 @@ import Home     from './routes/home'
 import Content  from './routes/content'
 import Userpage from './routes/userpage'
 
+const store = createStore(reducers)
+
 const App = () =>
-    <Router>
-        <Home     path="/" />
-        <Content  path="/content/:category_id?/:thread_id?/:comment_id?" />
-        <Userpage path="/user/:user_id" />
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <Home     path="/" />
+            <Content  path="/content/:category_id?/:thread_id?/:comment_id?" />
+            <Userpage path="/user/:user_id" />
+        </Router>
+    </Provider>
 
 export default App;
